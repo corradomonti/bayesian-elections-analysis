8b1d0314188ec87269bb48c2209b79a8611c8489
Corrado <corrado.mont@gmail.com>
2019-06-06 11:45:39 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bayesian-elections-analysis
---
+++ data/download_data.sh
@@ -0,0 +1,7 @@
+wget https://elezioni.interno.gov.it/daithome/documenti/europee2019_scrutini_area_italia.csv
+wget https://dait.interno.gov.it/documenti/europee2019_elettori_votanti_italia.csv
+wget https://raw.githubusercontent.com/ondata/elezionipolitiche2018/master/dati/scrutiniCI_cm.csv
+wget https://www1.finanze.gov.it/finanze3/analisi_stat/v_4_0_0/contenuti/Redditi_e_principali_variabili_IRPEF_su_base_comunale_CSV_2017.zip
+unzip Redditi_e_principali_variabili_IRPEF_su_base_comunale_CSV_2017.zip
+wget https://raw.githubusercontent.com/ondata/elezionipolitiche2018/master/risorse/comuniViminaleISTAT.csv
+
