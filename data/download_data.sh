8458b096643c0835e4dd38878da67df4148923a7
Corrado <corrado.mont@gmail.com>
2019-06-06 12:58:41 +0200

    File ufficiale dati politiche 2018

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,3 +1,4 @@
+wget https://dait.interno.gov.it/documenti/camera_2018_scrutini_italia.csv
 wget https://elezioni.interno.gov.it/daithome/documenti/europee2019_scrutini_area_italia.csv
 wget https://dait.interno.gov.it/documenti/europee2019_elettori_votanti_italia.csv
 wget https://raw.githubusercontent.com/ondata/elezionipolitiche2018/master/dati/scrutiniCI_cm.csv
